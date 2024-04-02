9eade36d9129247614186601361372c06917c529
Carlo Negroni <c.negroni01@ufromail.cl>
2024-04-02 18:11:44 -0300

    agregado de test

---
+++ src/test/java/com/example/MainTest.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+
+public class MainTest {
+    @Test
+    void testDivision() {
+
+    }
+
+    @Test
+    void testMultiplicacion() {
+
+    }
+
+    @Test
+    void testRecibirOpcion() {
+
+    }
+
+    @Test
+    void testResta() {
+
+    }
+
+    @Test
+    void testSuma() {
+
+    }
+}
